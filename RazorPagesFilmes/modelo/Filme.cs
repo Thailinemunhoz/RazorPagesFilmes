3560bbadd568c4add3ce13e6fb054896eeec6bf0
Thailine <Thailine@ATENDIMENTO>
2024-03-11 15:36:00 -0300

    Adicionar arquivos de projeto.

---
+++ RazorPagesFilmes/modelo/Filme.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesFilmes.modelo
+{
+    public class Filme
+    {
+        public int ID { get; set; }
+
+        public string Titulo { get; set; } = string.Empty;
+
+        [DataType(DataType.Date)]
+            
+        public DateTime DataLancamento { get; set; }
+
+        public string Genero { get; set; } = string.Empty;
+
+        public decimal reco { get; set; }
+
+    }
+}
